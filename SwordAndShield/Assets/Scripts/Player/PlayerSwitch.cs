233ea09b52f9f3eafb7abcd3ae375f4d84270dbf
Suat Martin <sgiraygonzal@umass.edu>
2023-07-08 11:07:52 -0700

    hiding and switching

--- SwordAndShield/Assets/Scripts/Player/PlayerSwitch.cs
+++ SwordAndShield/Assets/Scripts/Player/PlayerSwitch.cs
@@ -11,11 +11,18 @@ public class PlayerSwitch : MonoBehaviour
     public bool player1Active = true;
     Vector3 player1Pos;
     Vector3 player2Pos;
+
+    Vector3 spawnPoint;
+    [SerializeField] private GameObject[] players;
     // Start is called before the first frame update
     void Start()
     {
+        PlayerMovement.enabled = true;
+        player2Movement.enabled = false;
+        spawnPoint = new Vector3(player1.position.x, player1.position.y, player1.position.z);
         player1Pos = new Vector3(player1.position.x, player1.position.y, player1.position.z);
         player2Pos = new Vector3(player2.position.x, player2.position.y, player2.position.z);
+        players[1].SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,16 +37,20 @@ public class PlayerSwitch : MonoBehaviour
         if(player1Active){
             PlayerMovement.enabled = false;
             player2Movement.enabled = true;
-            player1Pos = player1.position;
-            player2.position = player1Pos;
+            spawnPoint = player1.position;
+            player2.position = spawnPoint;
             player1.position = player2Pos;
+            players[1].SetActive(true);
+            players[0].SetActive(false);
             player1Active = false;
         } else {
             PlayerMovement.enabled = true;
             player2Movement.enabled = false;
-            player2Pos = player2.position;
+            spawnPoint = player2.position;
+            player1.position = spawnPoint;
             player2.position = player1Pos;
-            player1.position = player2Pos;
+            players[1].SetActive(false);
+            players[0].SetActive(true);
             player1Active = true;
         }
     }
