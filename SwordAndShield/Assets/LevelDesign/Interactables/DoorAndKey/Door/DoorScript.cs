2cefb82941f1d819236f1f2b2f3db01bf3d699f6
Shuntoon <83526171+Shuntoon@users.noreply.github.com>
2023-07-08 22:01:24 -0700

    Started Functionality Shite

--- SwordAndShield/Assets/LevelDesign/Interactables/DoorAndKey/Door/DoorScript.cs
+++ SwordAndShield/Assets/LevelDesign/Interactables/DoorAndKey/Door/DoorScript.cs
@@ -17,12 +17,12 @@ public class DoorScript : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        /*if (collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("player"))
         {
-            if (collision.gameObject.GetComponent<playerGame>().keyCount > 0)
+            /*if (collision.gameObject.GetComponent<playerGame>().keyCount > 0)
             {
                 Destroy(this);
-            }
-        }*/
+            }*/
+        }
     }
 }
--- SwordAndShield/Assets/LevelDesign/Interactables/DoorAndKey/Key/KeyScript.cs
+++ SwordAndShield/Assets/LevelDesign/Interactables/DoorAndKey/Key/KeyScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
-    public GameObject player;
+    //public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +19,29 @@ public class KeyScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
+            print("Hi");
             //var player = collision.gameObject.GetComponent<playerScript>().AddToKeyCount;
             Destroy(this);
+        }
+        else
+        {
+            print("Bye");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print("Hi");
+            //var player = collision.gameObject.GetComponent<playerScript>().AddToKeyCount;
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Bye");
         }
     }
 }
